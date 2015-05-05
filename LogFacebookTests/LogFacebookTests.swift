05620add54e33e833c582e93fcc64c54bf0a21b9
Ihashi <ihashi91@yahoo.fr>
2015-05-05 15:38:51 +0200

    Initial Commit

---
+++ LogFacebookTests/LogFacebookTests.swift
@@ -0,0 +1,36 @@
+//
+//  LogFacebookTests.swift
+//  LogFacebookTests
+//
+//  Created by ESIEA on 05/05/2015.
+//  Copyright (c) 2015 HaiNguyen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LogFacebookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
